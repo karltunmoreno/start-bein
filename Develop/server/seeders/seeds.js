0c45b3ecec99774df7a4f895bbfbb6af471bac40
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-05-26 23:50:30 -0700

    compilation error

--- Develop/server/seeders/seeds.js
+++ Develop/server/seeders/seeds.js
@@ -7,6 +7,18 @@ const { User } = require('../models');
 db.once('open', async () => {
     await User.deleteMany({});
 
+    //CREATE USER DATA
+        //CREATE USER DATA
+        const userData = [];
 
-
+        for (let i = 0; i < 50; i +=1) {
+            const username = faker.internet.userName();
+            const email = faker.internt.email(username);
+            const password = faker.internet.password();
+    
+            userData.push({ username, email, password });
+        }
+    
+        const createdUsers = await User.collection.insertMany(userData);
+    
 })

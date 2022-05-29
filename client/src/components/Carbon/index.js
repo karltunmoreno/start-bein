685c98f1efae25f7f9df97b15edfdc775ef56c16
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-05-29 05:31:30 -0700

    Carbon file changed to Carbon directory with index.js best practice structure

--- client/src/components/Carbon/index.js
+++ client/src/components/Carbon/index.js
@@ -5,7 +5,7 @@ function Carbon() {
     return (
         <section>
             <h1 id="carbon">Your Carbon Footprint</h1>
-            <img src={coverImage} className="my-2" style={{ width: "100%", opacity: 0.4 }} alt="purple water lily" />
+         
         </section>
     );
 }
